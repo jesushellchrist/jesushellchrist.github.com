67ebcc1072b2dae49bdc79c8ae015cca9e1aed20
jesushellchrist <jesushellchrist@outlook.com>
2025-04-21 02:53:55 +0100

    Update and rename Scripts/Booga Hub Dragon Soul Free.lua to Unscriptify BABFT AutoFarm.lua

--- Scripts/Booga Hub Dragon Soul Free.lua
+++
@@ -1,3 +0,0 @@
-getgenv().PcUser = true -- put false if not a pc user
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FortniBloxYT1/Dragon-Soul-Booga-Hub-Free/refs/heads/main/Booga%20Key%20System.lua"))()
